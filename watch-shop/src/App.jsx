74f7c04d1519c0e58c20537e2126983b727f8ab9
sapna84 <sapna842004@gmail.com>
2026-08-19 17:37:17 +0530

    setting up react

---
+++ watch-shop/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-900">
+        Sri Adi Ananta
+      </h1>
+    </div>
+  );
+}
+
+export default App;
